74d50779ea5da3129d646e90c3855e2c13d24aa1
Kaleb Walton <kalebwalton@gmail.com>
2009-05-10 13:08:39 -0400

    Reverted reorder extension changes - it should be site scoped already because it's scoped down per page anyway.

--- lib/vhost/reorder_extensions.rb
+++
@@ -1,9 +0,0 @@
-module Vhost::ReorderExtensions
-  module Page
-    def self.included(base)
-      base.class_eval {
-        acts_as_list :scope => [:parent_id, :site_id]
-      }
-    end
-  end
-end
