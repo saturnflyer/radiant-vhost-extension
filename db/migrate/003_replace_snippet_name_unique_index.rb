334dba7542fd9242f35f6e55a31d1f8f5a4ab98e
Kaleb Walton <kalebwalton@gmail.com>
2009-05-30 15:56:38 -0400

    Removed Snippet's unique index on name, replaced with unique index on name + site_id

---
+++ db/migrate/003_replace_snippet_name_unique_index.rb
@@ -0,0 +1,12 @@
+class ReplaceSnippetNameUniqueIndex < ActiveRecord::Migration
+  def self.up
+    remove_index :snippets, [:name], :name => "name"
+    add_index :snippets, [:name, :site_id], :unique => true
+  end
+  
+  
+  def self.down
+    remove_index :snippets, [:name, :site_id]
+    add_index "snippets", ["name"], :name => "name", :unique => true
+  end
+end
