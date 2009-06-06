6eb75bf00a23d812f5f57bf04e62a3d69c95a721
Kaleb Walton <kalebwalton@gmail.com>
2009-06-05 23:14:56 -0400

    .

--- lib/vhost/managed_file_extensions.rb
+++ lib/vhost/managed_file_extensions.rb
@@ -3,13 +3,13 @@ module Vhost::ManagedFileExtensions
     file_system_path = (thumbnail ? thumbnail_class : self).attachment_options[:path_prefix]
     filename = case self.thumbnail
       when "thumb"
-        File.join(RAILS_ROOT, file_system_path, current_site.id.to_s, 'thumbs', thumbnail_name_for(true))
+        File.join(RAILS_ROOT, file_system_path, current_site.id.to_s, 'thumbs', thumbnail_name_for("thumb"))
       else
-        File.join(RAILS_ROOT, file_system_path, current_site.id.to_s, thumbnail_name_for(false))
+        File.join(RAILS_ROOT, file_system_path, current_site.id.to_s, thumbnail_name_for)
     end
 
     if thumbnail
-      filename = File.join(RAILS_ROOT, file_system_path, current_site.id.to_s, 'thumbs', thumbnail_name_for(true))
+      filename = File.join(RAILS_ROOT, file_system_path, current_site.id.to_s, 'thumbs', thumbnail_name_for("thumb"))
     end
     
     filename
