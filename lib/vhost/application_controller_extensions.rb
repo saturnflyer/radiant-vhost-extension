d7099988c763da8483a87ce2817a706e97350859
Kaleb Walton <kalebwalton@gmail.com>
2009-08-16 11:35:11 -0400

    Updated vhost redirect

--- lib/vhost/application_controller_extensions.rb
+++ lib/vhost/application_controller_extensions.rb
@@ -3,24 +3,32 @@ module Vhost::ApplicationControllerExtensions
     base.class_eval {
       prepend_before_filter :redirect_to_primary_site
 
+      helper_method :primary_site_url
+      
       def redirect_to_primary_site
         if VhostExtension.REDIRECT_TO_PRIMARY_SITE
           site = current_site
           return if site.nil? || site.hostname.include?("*")
-          
+          primary_host = site.hostname.split(',')[0].strip
+          redirect_to(primary_site_url + request.request_uri) if request.host != primary_host
+        end
+      end
+
+      def primary_site_url
+          site = current_site
+          return nil if site.nil? || site.hostname.include?("*")
+
           # Rebuild the current URL. Check if it matches the URL of the
           # primary site and redirect if it does not.
           prefix = request.ssl? ? "https://" : "http://"
           host = request.host
           port = request.port_string
-          uri = request.request_uri
 
           # Primary site is the first site
           primary_host = site.hostname.split(',')[0].strip
-          full_url = prefix+primary_host+port+uri
           
-          redirect_to(full_url) if host != primary_host
-        end
+          # Return the concatenation
+          prefix+primary_host+port
       end
     }
   end
