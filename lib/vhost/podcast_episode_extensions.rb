f3ed542a53ede12f0e5cd43ba591327ad058b08a
Jim Gay <jim@saturnflyer.com>
2010-05-31 20:38:21 -0400

    remove extension specific options

--- lib/vhost/podcast_episode_extensions.rb
+++
@@ -1,6 +0,0 @@
-module Vhost::PodcastEpisodeExtensions
- def full_filename(thumbnail = nil)
-    file_system_path = self.attachment_options[:path_prefix]
-    File.join(RAILS_ROOT, file_system_path, current_site.id.to_s, podcast.id.to_s, thumbnail_name_for)
-  end
-end
