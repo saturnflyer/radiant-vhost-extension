1b7b3d21b1118daddbc30c87e32ded534ec52053
Jim Gay <jim@saturnflyer.com>
2010-05-31 20:45:06 -0400

    site_admin? is only site_admins

--- lib/vhost/application_helper_extensions.rb
+++ lib/vhost/application_helper_extensions.rb
@@ -1,11 +1,11 @@
 module Vhost::ApplicationHelperExtensions
   def self.included(receiver)
-    # Looks like this swaps out the 'subtitle' method for the 'site_hostname'
+    # This swaps out the 'subtitle' method for the 'site_hostname'
     # method to show the hostname in the subtitle in admin...
     receiver.send :alias_method_chain, :subtitle, :site_hostname
     
     receiver.send :define_method, :site_admin? do
-      current_user and (current_user.site_admin? or current_user.admin?)
+      current_user and current_user.site_admin?
     end
   end
   
