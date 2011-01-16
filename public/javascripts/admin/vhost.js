59ccede6b4e7aab2e573cd8a9f4f60d1c5946d90
Ryan Krug <ryan.krug@gmail.com>
2011-01-16 18:13:52 +0000

    be a bit more careful when creating custom domain names

--- public/javascripts/admin/vhost.js
+++ public/javascripts/admin/vhost.js
@@ -1,22 +1,39 @@
 document.observe('dom:loaded',function(){
+	$('site_title').observe('keyup', function(event) {
+		var input = $('site_hostnames_attributes_0_domain')
+		if (input && input.value != '*') {
+			var stext = this.value.toLowerCase()
+			stext = stext.gsub(/[^a-z0-9-]/, '')		
+			stext = stext + ".kuviat.com"
+			input.value = stext
+		}
+	})
   
   $('hostnames').insert({
     after: new Element('p').update(new Element('a',{href: '#', id: 'add_domain'}).update('Add domain')) 
   })
   
-  $$('p.hostname').each(function(item){ 
-    item.insert({ 
-      bottom: new Element('img', {src: '/images/admin/minus.png', className: 'domain_remover'})
-    }); 
+	$$('p.hostname').each(function(item, index){ 
+		if (index == 0) {
+			item.down('input.domain').setAttribute('disabled', 'disabled');
+		}	else {
+			item.insert({ 
+				bottom: new Element('img', {src: '/images/admin/minus.png', className: 'domain_remover'})
+			}); 
+		}
+
   });
   
   Event.addBehavior({  
     '#add_domain:click': function(){  
       var new_hostname = $$('#hostnames p.hostname')[0].cloneNode(true)
       var label = $(new_hostname).down('label'); $(label).writeAttribute('id','')
-      var input = $(new_hostname).down('input.domain'); $(input).writeAttribute('id','')
+			var input = $(new_hostname).down('input.domain'); $(input).writeAttribute('id',''); $(input).removeAttribute('disabled'); $(input).value = ''
       var destroy = $(new_hostname).down('input.delete_input').remove();
       var new_hostname_id = $(input).identify();
+			new_hostname.insert({ 
+				bottom: new Element('img', {src: '/images/admin/minus.png', className: 'domain_remover'})
+			}); 
       $(label).writeAttribute('for',new_hostname_id)
       $(input).writeAttribute('name','site[hostnames_attributes]['+ $$('input[name*=domain]').size() + '][domain]')
       $(input).writeAttribute('value','')
