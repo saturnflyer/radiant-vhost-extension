d9d93f0f7c4d8ef4d63b96f8abeb313a17a8d99f
Edmund Haselwanter <edmund@haselwanter.com>
2011-08-31 21:54:23 +0200

    changed observer

--- app/models/site_association_observer.rb
+++ app/models/site_association_observer.rb
@@ -1,5 +1,7 @@
+require 'vhost_extension'
 class SiteAssociationObserver < ActiveRecord::Observer
-  observe *VhostExtension::SITE_SPECIFIC_MODELS.collect(&:constantize)
+  # observe *VhostExtension::SITE_SPECIFIC_MODELS.collect(&:constantize)
+  observe *VhostExtension.MODELS.collect(&:constantize)
   
   def before_validation(model)
     # Set the site_id automatically if it's not already set 
