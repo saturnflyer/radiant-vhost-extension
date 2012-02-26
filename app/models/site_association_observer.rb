bc3a3e9f58423d903a7cecc8d22d51b5d725be90
Jim Gay <jim@saturnflyer.com>
2012-02-26 00:14:16 -0500

    Use the MODELS collection for observing

--- app/models/site_association_observer.rb
+++ app/models/site_association_observer.rb
@@ -1,5 +1,5 @@
 class SiteAssociationObserver < ActiveRecord::Observer
-  observe *VhostExtension::SITE_SPECIFIC_MODELS.collect(&:constantize)
+  observe *VhostExtension.MODELS.collect(&:constantize)
   
   def before_validation(model)
     # Set the site_id automatically if it's not already set 
